b863cdd2d17e14b3350cf9e4f076887d411d0115
Andreas Themistocles <andreasthemistocles@hotmail.com>
2022-10-30 20:20:59 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+/* db.js | Andreas Themistocles | 301251197 | COMP-229-Midterm */
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  //"URI" : "mongodb://localhost/books"
+  "URI" : "mongodb+srv://Andreas:rAW8UikAQHUzYrt4@midtermdbserver.ir7e7cl.mongodb.net/books?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,16 @@
+/* books.js | Andreas Themistocles | 301251197 | COMP-229-Midterm */
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
